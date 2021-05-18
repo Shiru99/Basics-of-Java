5c1c10ee0bb4cc9e70da588b44a323ca464a8ea0
Shiru99 <shriram.ghadge1999@gmail.com>
2021-05-18 16:55:41 +0530

    Multi-Threading added

---
+++ MultiThreading.java
@@ -0,0 +1,14 @@
+///////////////////////////////  Multi-Threading  //////////////////////////////
+/*
+
+    MULTITHREADING (executing two or more threads simultaneously to maximum utilization of CPU)
+
+        1. Multithreaded applications execute two or more threads run concurrently. Hence, it is also known as Concurrency in Java. Each thread runs parallel to each other. 
+
+        2. Mulitple threads don't allocate separate memory area, hence they save memory. Also, context switching between threads takes less time.
+
+*/
+
+public class MultiThreading {
+    
+}
