4ed9c9b226d0b6517ac5485c7b7ca8a5daf60205
Shiru99 <shriram.ghadge1999@gmail.com>
2021-05-18 17:15:48 +0530

    Multi-Threading added

--- MultiThreading.java
+++ MultiThreading.java
@@ -10,7 +10,7 @@
         4. Waiting
         5. Dead
 
-        
+
 
     MULTITHREADING (executing two or more threads simultaneously to maximum utilization of CPU)
 
@@ -23,6 +23,38 @@
 
 */
 
-public class MultiThreading {
+class ThreadOne extends Thread {
+    @Override
+    public void run() {
+        super.run();
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Thread : 1");
+        }
     
+    }
+}
+
+class ThreadTwo extends Thread {
+    @Override
+    public void run() {
+        super.run();
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Thread : 2");
+        }
+
+    }
+}
+
+public class MultiThreading {
+    public static void main(String[] args) {
+
+        Thread threadOne = new ThreadOne();
+        Thread threadTwo = new ThreadTwo();
+
+        threadOne.start();
+        threadTwo.start();
+
+    }
 }
