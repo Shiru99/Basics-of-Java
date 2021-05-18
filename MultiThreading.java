924af5b8731101b6b394dbdc0b44db153ec27e0f
Shiru99 <shriram.ghadge1999@gmail.com>
2021-05-18 17:07:25 +0530

    Multi-Threading added

--- MultiThreading.java
+++ MultiThreading.java
@@ -1,11 +1,25 @@
 ///////////////////////////////  Multi-Threading  //////////////////////////////
 /*
 
+
+    Various stages of life cycle of thread
+
+        1. New
+        2. Runnable
+        3. Running
+        4. Waiting
+        5. Dead
+
+        
+
     MULTITHREADING (executing two or more threads simultaneously to maximum utilization of CPU)
 
         1. Multithreaded applications execute two or more threads run concurrently. Hence, it is also known as Concurrency in Java. Each thread runs parallel to each other. 
 
         2. Mulitple threads don't allocate separate memory area, hence they save memory. Also, context switching between threads takes less time.
+
+
+    
 
 */
 
