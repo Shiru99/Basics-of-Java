2277fc7b252aba2f7b8052f6212923cae217344d
Shiru99 <shriram.ghadge1999@gmail.com>
2021-05-26 19:40:10 +0530

    Generics

---
+++ Generic.java
@@ -0,0 +1,26 @@
+class Gene<X,Y> 
+{
+    X a;
+    Y b;
+
+    public Gene(X x, Y y) {
+        this.a = x;
+        this.b = y;
+    }
+
+    public void description() {
+        System.out.println( a.getClass().getName() + " - " + this.a + " & " +
+                            b.getClass().getSimpleName() + " - " + this.b );
+    }
+}
+
+public class Generic {
+    public static void main(String[] args) 
+    {
+        Gene<String, Integer> g1 = new Gene<String, Integer>("John Doe", 33);
+        Gene<Boolean, Double> g2 = new Gene<>(true, 3.14);
+
+        g1.description();
+        g2.description();
+    }
+}
