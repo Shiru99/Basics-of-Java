03d5661b149099a523994f6d53ea0ecf3515688b
Shiru99 <shriram.ghadge1999@gmail.com>
2021-05-27 09:30:31 +0530

    Generics

--- Generic.java
+++ Generic.java
@@ -8,6 +8,22 @@ class Gene<X,Y>
         this.b = y;
     }
 
+    public X getA() {
+        return a;
+    }
+
+    public void setA(X a) {
+        this.a = a;
+    }
+
+    public Y getB() {
+        return b;
+    }
+
+    public void setB(Y b) {
+        this.b = b;
+    }
+
     public void description() {
         System.out.println( a.getClass().getName() + " - " + this.a + " & " +
                             b.getClass().getSimpleName() + " - " + this.b );
