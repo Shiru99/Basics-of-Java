79805f40e06114c064afd1ddaa57ef0c7736d9af
Shiru99 <shriram.ghadge1999@gmail.com>
2021-05-27 09:26:19 +0530

    Generics

--- Generic.java
+++ Generic.java
@@ -15,12 +15,19 @@ class Gene<X,Y>
 }
 
 public class Generic {
+    
     public static void main(String[] args) 
     {
-        Gene<String, Integer> g1 = new Gene<String, Integer>("John Doe", 33);
-        Gene<Boolean, Double> g2 = new Gene<>(true, 3.14);
+        // Gene<String,Integer> g1 = new Gene<String,Integer>("John Doe", 33);
+        // Gene<Boolean,Double> g2 = new Gene<>(true, 3.14);
 
-        g1.description();
-        g2.description();
+        // g1.description();
+        // g2.description();
+
+        Sum(2,4);
+    }
+
+    static <A,B> void Sum(A a,B b){
+        System.out.println(a+","+b);
     }
 }
