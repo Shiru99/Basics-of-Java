48ef6f5b8da772064964fd6c00e6857273214db7
Shiru99 <shriram.ghadge1999@gmail.com>
2021-06-05 08:02:00 +0530

    Generics - Lower bound wildccard

--- Generics/GenericINInheritance.java
+++ Generics/GenericINInheritance.java
@@ -8,7 +8,7 @@ public class GenericINInheritance {
     private static void printListWithoutGenerics(List l){
         l.add("Anything int/String");
         l.forEach(System.out::println);
-        System.out.println();
+        System.out.println("--------");
         l.remove("Anything int/String");
 
         Object a = l.get(0);
@@ -17,7 +17,7 @@ public class GenericINInheritance {
     /*
         String/Integer is inherited from Object
 
-        Generics for Integer/String doesn't accept Object (parent-type)
+        Generics for Integer/String doesn't accept Object (parent-type) i.e. generics can't be inherited.
     */
 
     // Error - can break type safety
@@ -34,12 +34,12 @@ public class GenericINInheritance {
 
     */
 
-    private static void printList(List<?> l){
+    private static void printList(List<?> l){   // READ only
 
         // l.add("Anything int/String");        // Error (point 2) 
         
         l.forEach(System.out::println);
-        System.out.println();
+        System.out.println("------------");
 
         Object a = l.get(0);
         // l.add(new Object());                 // Error
@@ -54,11 +54,39 @@ public class GenericINInheritance {
         // l.add("Anything int/String");        // Error (point 2)
         // l.add(99);                           // Error
         l.forEach(System.out::println);
-        System.out.println();
+        System.out.println("------------");
 
         Object a = l.get(0);
         Number b = l.get(0);
         // l.add(new Object());                 // Error
+    }
+
+    /* 
+        Lower bound Wildcard - <? super --- >     // allow WRITE in any super of mentioned 
+
+        ? super Number ==> Allows write in super of Numbers but not in special like int/double
+        ? super Integer ==> Allows write in super of Integer - Integer/Number
+
+    */
+    private static void addNumbers(List<? super Number>l,Number n){
+        
+        // WRITE :
+
+        l.add(99);
+        l.add(2.2);
+        // l.add("Fun");                    // Error
+        // l.add(new Object());             // Error
+
+        // READ :
+
+        l.forEach(System.out::println);
+        System.out.println("------------");
+
+        Object a = l.get(0);
+        // Number b = l.get(0);         // Error - Type MisMatch
+        System.out.println(l.get(0).getClass().getSimpleName());    // 0.0 - Double
+        System.out.println(l.get(1).getClass().getSimpleName());    // 9 - Integer
+
     }
 
     public static void main(String[] args) {
@@ -69,13 +97,15 @@ public class GenericINInheritance {
         printListWithoutGenerics(names);
         printList(names);
         // printNumbers(names);     // Error
+        // addNumbers(names,3);     // Error
 
-        List<Integer> nums = new ArrayList<>();
-        nums.add(0);
-        nums.add(99);
-        printListWithoutGenerics(nums);
-        printList(nums);
-        printNumbers(nums);
+        List<Integer> integers = new ArrayList<>();
+        integers.add(0);
+        integers.add(99);
+        printListWithoutGenerics(integers);
+        printList(integers);
+        printNumbers(integers);
+        // addNumbers(integers,3);      // Error
 
         List<Double> doubles = new ArrayList<>();
         doubles.add(0.0);
@@ -83,6 +113,15 @@ public class GenericINInheritance {
         printListWithoutGenerics(doubles);
         printList(doubles);
         printList(doubles);
+        // addNumbers(doubles,3);      // Error
+
+        List<Number> nums = new ArrayList<>();
+        nums.add(0.0);
+        nums.add(9);
+        printListWithoutGenerics(nums);
+        printList(nums);
+        printList(nums);
+        addNumbers(nums,3.3);      // Error
 
     }
 }
