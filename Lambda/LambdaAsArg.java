aa0a0631e62fb8b78fb10f75acf91f2c84ac0b5d
Shiru99 <shriram.ghadge1999@gmail.com>
2021-06-07 11:19:18 +0530

    Target Type - Lambda

--- Lambda/LambdaAsArg.java
+++ Lambda/LambdaAsArg.java
@@ -1,5 +1,14 @@
 package Lambda;
 
+
+/*
+
+    A functional Interface for which a lambda expression has invoked is called "target type" of lambda expression. 
+    
+    It means that if a lambda expression has invoked for some "X" interface then "X" is the target type of that lambda expression. Hence, we conclude that lambda expressions can be used only in those situations where java compiler can determine the Target Type.
+*/
+
+
 import java.util.*;
 
 interface Condition{
@@ -28,6 +37,9 @@ public class LambdaAsArg {
     }
 
     private static void printPpl(List<Person> ppl,Condition cond) {
+
+        System.out.println(cond.getClass().getSimpleName());
+
         for (Person person : ppl) {
             if(cond.isSuitable(person)){
                 System.out.println(person);
