4e818cbca953eff45f5831130781fb0641e759eb
Shiru99 <shriram.ghadge1999@gmail.com>
2022-07-18 11:38:51 +0530

    Annotations

--- Annotations.java
+++ Annotations.java
@@ -8,9 +8,6 @@
         @SuppressWarnings
         @FunctionalInterface
 
-
-    A functional interface is an interface that contains only one abstract method. They can have only one functionality to exhibit. From Java 8 onwards, lambda expressions can be used to represent the instance of a functional interface
-
 */
 
 @FunctionalInterface
