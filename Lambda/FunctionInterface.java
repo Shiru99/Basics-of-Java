69798384df6f01988e904d8c04e56850d041d214
Shiru99 <shriram.ghadge1999@gmail.com>
2022-07-19 13:10:11 +0530

    predefined functional interfaces

--- Lambda/FunctionInterface.java
+++ Lambda/FunctionInterface.java
@@ -1,36 +1,37 @@
-/*
+/* 3
     docs.oracle.com/javase/8/docs/api/java/util/function/package-summary.html
 
-    Java 8 has defined a lot of functional interfaces to be used extensively in lambda expressions
-    so need to create new functional interface
+    Java 8 has defined a lot of functional interfaces to be used extensively in lambda expressions so no need to create new functional interface
 
 */
 
 package Lambda;
 
 import java.util.*;
-import java.util.function.*;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class FunctionInterface {
     public static void main(String[] args) {
         List<Person> ppl = Arrays.asList(
-            new Person("Tiger", "wood", 33),
-            new Person("John", "Doe", 32),
-            new Person("Jenny", "Ccc", 28),
-            new Person("James", "Eee", 31),
-            new Person("joe", "Eee", 32)
-        );
+                new Person("Tiger", "wood", 33),
+                new Person("John", "Doe", 32),
+                new Person("Jenny", "Ccc", 28),
+                new Person("James", "Eee", 31),
+                new Person("joe", "Eee", 32));
 
-        printPpl(ppl,(p)->true,(p)-> System.out.println(p));
+        printPpl(ppl, (p) -> true, (p) -> System.out.println(p));
         System.out.println("-----");
-        printPpl(ppl, (p)-> p.getFirstName().startsWith("J"),p-> System.out.println(p.getFirstName()));
+
+        printPpl(ppl, (p) -> p.getFirstName().startsWith("J"), p -> System.out.println(p.getFirstName()));
         System.out.println("-----");
-        printPpl(ppl, (p)-> p.getAge()<30,p->System.out.println(p.getAge()+" "+p.getFirstName()));
+
+        printPpl(ppl, (p) -> p.getAge() < 30, p -> System.out.println(p.getAge() + " " + p.getFirstName()));
     }
 
-    private static void printPpl(List<Person> ppl,Predicate<Person> predict,Consumer<Person> printDetails) {
+    private static void printPpl(List<Person> ppl, Predicate<Person> predict, Consumer<Person> printDetails) {
         for (Person person : ppl) {
-            if(predict.test(person)){
+            if (predict.test(person)) {
                 printDetails.accept(person);
             }
         }
