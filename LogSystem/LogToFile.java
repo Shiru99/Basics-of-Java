e3c57927ca82c2417c64ee6e0e63745366e80478
Shiru99 <shriram.ghadge1999@gmail.com>
2022-07-29 08:40:49 +0530

    Log - File Handler - Formatter & Filter

--- LogSystem/LogToFile.java
+++ LogSystem/LogToFile.java
@@ -2,6 +2,7 @@ package LogSystem;
 
 import java.io.IOException;
 import java.util.logging.FileHandler;
+import java.util.logging.Filter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
@@ -14,9 +15,14 @@ public class LogToFile {
 
         try {
             LOGGER.setLevel(Level.FINER); 
-            FileHandler fileHandler = new FileHandler("LogSystem/Logs/LogToFile.log", false); 
+            FileHandler fileHandler = new FileHandler("LogSystem/Logs/LogsToFile.log", false); 
             // true - append to existing file
             fileHandler.setFormatter(new SimpleFormatter());
+
+            Filter filter = log -> log.getLevel().intValue() >= Level.WARNING.intValue();
+            fileHandler.setFilter(filter);
+
+
             fileHandler.setLevel(Level.FINER); 
             LOGGER.addHandler(fileHandler);
         } catch (SecurityException | IOException e) {
@@ -27,14 +33,11 @@ public class LogToFile {
     
     public static void main(String[] args) {
 
-        // LOGGER.log(Level.SEVERE, "Log - Hello World");
-        // LOGGER.log(Level.WARNING, "Log - Hello World");
-        // LOGGER.log(Level.INFO, "Log - Hello World");
-        // LOGGER.log(Level.FINER, "Log - Hello World");
-
-        /* Console Handler */
+        /* Console Handler + File Handler */
         
 
+        LOGGER.log(Level.SEVERE, "SEVERE Log - Hello World");
+        LOGGER.log(Level.WARNING, "WARNING Log - Hello World");
         LOGGER.log(Level.INFO, "INFO Log - Hello World");
         LOGGER.log(Level.FINER, "FINER Log - Hello World");
         LOGGER.log(Level.FINEST, "FINEST Log - Hello World");
